9b0a4dff1938e07db773ae1bbe6577b702c97f21
SebaPlatero <167647458+SebaPlatero@users.noreply.github.com>
2024-05-16 21:32:35 -0300

    con_race_condition.c

--- TP3/con_race_condition.c
+++ TP3/con_race_condition.c
@@ -4,13 +4,13 @@
 #define NUMBER_OF_THREADS 2
 #define CANTIDAD_INICIAL_HAMBURGUESAS 20
 int cantidad_restante_hamburguesas = CANTIDAD_INICIAL_HAMBURGUESAS;
-
+int turno = 0;
 
 void *comer_hamburguesa(void *tid)
 {
 	while (1 == 1)
 	{ 
-		
+		while(turno!=(int)tid);
     // INICIO DE LA ZONA CRÍTICA
 		if (cantidad_restante_hamburguesas > 0)
 		{
@@ -23,6 +23,7 @@ void *comer_hamburguesa(void *tid)
 
 			pthread_exit(NULL); // forzar terminacion del hilo
 		}
+		turno = (turno + 1)% NUMBER_OF_THREADS;
     // SALIDA DE LA ZONA CRÍTICA   
 
 	}
