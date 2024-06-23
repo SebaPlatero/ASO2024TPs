e2b410f590690cb2b27b3c735df2447b5a46aa7d
SebaPlatero <sebaplatero1197@gmail.com>
2024-06-23 17:31:33 -0300

    nuevo

---
+++ TP5/ArqParte1.sh
@@ -0,0 +1,54 @@
+function suma {
+        resultado=$(echo "$1 + $2" | bc)
+
+}
+
+function resta {
+        resultado=$(echo "$1 - $2" | bc)
+
+}
+
+function multiplicacion {
+        resultado=$(echo "$1 * $2" | bc)
+
+}
+
+function division {
+        resultado=$(echo "scale=2; $1 / $2" | bc)
+
+}
+
+echo "Bienvenido a la calculadora simple!"
+echo "Elige la operación que deseas realizar:"
+echo "1. Suma"
+echo "2. Resta"
+echo "3. Multiplicación"
+echo "4. División"
+read -p "Elige tu opcion: " opcion
+
+read -p "Introduce el primer número: " num1
+read -p "Introduce el segundo número: " num2
+
+case $opcion in
+    1)
+        suma $num1 $num2
+        resultado=$(( num1 + num2 ))
+echo "Resultado de la suma: $resultado"
+        ;;
+    2)
+        resta $num1 $num2
+        resultado=$(( num1 - num2 ))
+echo "Resultado de la resta: $resultado"
+        ;;
+    3)
+        multiplicacion $num1 $num2
+        resultado=$(( num1 * num2 ))
+echo "Resultado de la multiplicacion: $resultado"
+        ;;
+    4)
+        division $num1 $num2
+        resultado=$(( num1 / num2 ))
+echo "Resultado de la division: $resultado"
+        ;;
+
+esac
